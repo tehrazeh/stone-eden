4946b1aee8ede1628a2ba6d1d69c3adeed2cfeea
tehrazeh <danilatolstoukhov@gmail.com>
2023-01-11 23:13:42 -0500

    Added classes filter with data

--- src/Components/Filter/Class/index.tsx
+++ src/Components/Filter/Class/index.tsx
@@ -1,6 +1,18 @@
+import { useAppSelector } from "../../../utils/hooks"
 const ClassFilter = () => {
+
+  const classes = useAppSelector(state => {
+    if ('classes' in state.info.info) {
+      return state.info.info.classes
+    }
+  })
+
   return (
-    <div>this is filter by class</div>
+    <>
+    {(classes) ? (classes.map((item, index) => { 
+      return <div key={index}>{item}</div>}))
+       : <h2>Loading...</h2>}
+    </>
   )
 }
 
--- src/Pages/Search.tsx
+++ src/Pages/Search.tsx
@@ -1,6 +1,17 @@
+import { useEffect } from "react"
 import { Outlet } from "react-router-dom"
+import { fetchInfo } from "../Redux/info/asyncActions"
+import { useAppDispatch, useAppSelector } from "../utils/hooks"
 const Search = () => {
 
+  const status = useAppSelector((state) => state.info.status)
+  const dispatch = useAppDispatch()
+  
+  useEffect(() => {
+    if(status === 'loading') {
+      dispatch(fetchInfo())
+    }
+  }, [status, dispatch])
   return (
     <>
     <Outlet/>   
