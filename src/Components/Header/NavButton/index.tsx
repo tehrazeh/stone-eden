c2292ce3b4e632f78d4f30967e4e9d0e92fce81c
tehrazeh <danilatolstoukhov@gmail.com>
2023-01-12 20:56:29 -0500

    Added reusable button classes and changed header component

--- src/Components/Header/NavButton/index.tsx
+++ src/Components/Header/NavButton/index.tsx
@@ -4,18 +4,9 @@ type NavButtonProps = {
     title: string
 }
  const NavButton: React.FC<NavButtonProps> = (props) => {
-
-  const activeStyle = `mx-4 text-emerald-200 uppercase bg-emerald-800
-  p-1 rounded transition-all duration-200 ease-linear w-20
-  flex justify-center items-center shadow-lg`
-
-  const regularStyle = `mx-4 text-emerald-300 p-1 uppercase bg-stone-700
-  rounded shadow-lg hover:text-emerald-200 hover:scale-125 transition-all
-  duration-200 ease-linear w-20 flex justify-center items-center`
-
   return (
     <NavLink to={`/search/${props.title.toLowerCase()}`}
-      className={({ isActive }) => isActive ? activeStyle : regularStyle }>
+      className={({ isActive }) => isActive ? 'button-active' : 'button-regular' }>
       {props.title}
     </NavLink>
   )
