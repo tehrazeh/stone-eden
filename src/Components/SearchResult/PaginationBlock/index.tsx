78227442823a2b00841194af67c94aacb86f9790
tehrazeh <danilatolstoukhov@gmail.com>
2023-02-09 14:29:00 -0500

    Added pagination buttons that change pages

--- src/Components/SearchResult/PaginationBlock/index.tsx
+++ src/Components/SearchResult/PaginationBlock/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react"
-import { setTotalItems, setTotalPages } from "../../../Redux/pagination/slice"
+import { setCurrentPage, setTotalItems, setTotalPages } from "../../../Redux/pagination/slice"
 import { useAppSelector, useAppDispatch } from "../../../utils/hooks"
 
 const Pagination = () => {
@@ -11,6 +11,18 @@ const Pagination = () => {
         dispatch(setTotalItems(cards.length))
     }, [cards, dispatch])
 
+    let pageButtons = []
+
+    if (totalPages > 1) {
+      for (let i = 0; i < totalPages; i++) {
+        pageButtons.push(
+        <button className='border-2 border-solid border-red-400 m-2 rounded-full'
+                key={i}
+                onClick={() => {dispatch(setCurrentPage(i))}}>
+          {i + 1}
+        </button>)
+      }
+    }
     
   return (
     <div>
@@ -23,6 +35,7 @@ const Pagination = () => {
 
           <button className='border-2 border-solid border-red-400 m-2'>{`>`}</button>
         </div> */}
+        {pageButtons}
     </div>
   )
 }
