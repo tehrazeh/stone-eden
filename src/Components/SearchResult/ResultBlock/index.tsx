f5762679b25624d42011c656553a4c8bf8e63fa5
tehrazeh <danilatolstoukhov@gmail.com>
2023-02-10 14:09:31 -0500

    Hid the pagination by default

--- src/Components/SearchResult/ResultBlock/index.tsx
+++ src/Components/SearchResult/ResultBlock/index.tsx
@@ -5,11 +5,6 @@ import Pagination from "../PaginationBlock"
 const ResultBlock = () => {
     const {data} = useAppSelector((state) => state.data)
     const {currentPage, elementsPerPage} = useAppSelector(state => state.pagination)
-    // const cards = data.filter((element, index, arr) => 
-    // // thanks! https://stackoverflow.com/questions/2218999/how-to-remove-all-duplicates-from-an-array-of-objects
-    // arr.findIndex(element2 => (element2.name===element.name)) === index).filter((item) => {
-    //   return item.type?.toLowerCase() !== 'enchantment' // temporarily avoid enchantments cause they are baggy
-    // })
 
     const cards = paginateArray(data, elementsPerPage, currentPage)
     
@@ -19,7 +14,7 @@ const ResultBlock = () => {
   return (
     <div className='bg-slate-500 w-full flex flex-wrap justify-center items-center flex-col'>
       <div>
-      <Pagination />
+      {data.length > 0 && <Pagination />}
       </div>
       <div className='flex flex-wrap'>
         {pageElements}
