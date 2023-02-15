c9abdabb7f72afa7e9087febff1de4ed4c236d49
tehrazeh <danilatolstoukhov@gmail.com>
2023-02-14 21:28:54 -0500

    Created usePagination hook that returns an array of values for buttons

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -7,10 +7,24 @@ export const useAppDispatch: () => AppDispatch = useDispatch
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 
 
-
+export const DOTS = '...'
 export const usePagination = (totalPages: number, currentPage: number) => {
     const pages = []
     for (let i = 1; i <= totalPages; i++) {
     pages.push(i)
 }
+    let displayedPages: Array<string | number> = [...pages]
+
+if (currentPage >= 1 && currentPage <= 3) { // [1, 2, 3 ... last page]
+    displayedPages = [1, 2, 3, 4, DOTS, totalPages]
+    } else if (currentPage >= 4 && currentPage < pages.length - 2) { // [1 ... 2, 3, 4 ... last page]
+    const slicedPortion = pages.slice(currentPage -2, currentPage + 1) // 3 middle buttons (2, 3, 4)^
+    // left DOTS betwen 1 and 2 bc of the screenshot on github
+    displayedPages = [1, DOTS, ...slicedPortion, DOTS, totalPages]
+    } else if (currentPage >= pages.length - 2) { // [1 ... last page -2, last page -1, last page]
+    const slicedPortion = pages.slice(pages.length - 4) // [last page -2, last page -1, last page]
+    displayedPages = [1, DOTS, ...slicedPortion]
+    }
+
+    return displayedPages
 }
