4365ff3f7d337c50e68c195a46f8b5ef78320cdc
tehrazeh <danilatolstoukhov@gmail.com>
2023-03-02 13:06:54 -0500

    Fixed update of dropdown property filter when the new search is executed

--- src/Components/SearchResult/ResultFilter/index.tsx
+++ src/Components/SearchResult/ResultFilter/index.tsx
@@ -1,9 +1,10 @@
 import { setDataFilter } from "../../../Redux/datafilter/slice";
 import { DataSort } from "../../../Redux/datafilter/types"
-import { useAppDispatch } from "../../../utils/hooks";
+import { useAppDispatch, useAppSelector } from "../../../utils/hooks";
 
 const ResultFilter = () => {
     const dispatch = useAppDispatch()
+    const sortFilter = useAppSelector(state => state.dataFilter.sortFilter)
   return (
     <div className="rounded bg-yellow-800 border-2 border-yellow-600 w-full flex justify-center items-start">
         <select className="rounded ring-0 w-8/12 h-8 bg-stone-800 border-solid border-2 m-2 border-stone-500 text-emerald-400
@@ -11,9 +12,9 @@ const ResultFilter = () => {
         onChange={(e) => {
           dispatch(setDataFilter(e.target.value))
           }}
-        >
-          {/* MAKE IT TO STAY AT SORT AFTER EACH NEW SEARCH */}
-        <option value="" selected>Sort</option>
+        value={sortFilter}
+        defaultValue={DataSort.DEFAULT}>
+        <option value={DataSort.DEFAULT}>Sort</option>
         <option value={DataSort.HEALTH_DESC}>health down</option>
         <option value={DataSort.HEALTH_ASC}>health up</option>
         <option value={DataSort.COST_DESC}>cost down</option>
