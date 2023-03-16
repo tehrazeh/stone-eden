d7c849d056ca62e1516562cc5a64b0c1051bd1fc
tehrazeh <danilatolstoukhov@gmail.com>
2023-03-15 23:33:03 -0400

    Changed data filter to list instead of select, added images instead of text

--- src/Components/SearchResult/ResultFilter/index.tsx
+++ src/Components/SearchResult/ResultFilter/index.tsx
@@ -1,27 +1,48 @@
+import { useState } from "react";
 import { setDataFilter } from "../../../Redux/datafilter/slice";
 import { DataSort } from "../../../Redux/datafilter/types"
+
 import { useAppDispatch, useAppSelector } from "../../../utils/hooks";
 
 const ResultFilter = () => {
-    const dispatch = useAppDispatch()
-    console.log(DataSort.HEALTH_DESC.split(' ')[0])
-    const sortFilter = useAppSelector(state => state.dataFilter.sortFilter)
+  const dispatch = useAppDispatch()
+  const options = [<li value={DataSort.DEFAULT}
+    onClick={(() => {
+      dispatch(setDataFilter((DataSort.DEFAULT)))
+    })} key={DataSort.DEFAULT}>Default</li>]
+  for (const value in DataSort) {
+    if (value !== "DEFAULT") {
+      options.push(
+        <li value={value} key={value} 
+        className="flex justify-around"
+        onClick={(() => {
+          dispatch(setDataFilter(DataSort[value as keyof typeof DataSort]))
+        })}>
+          <img src={require(`../../../Assets/Attributes/${value.split('_')[0].toLowerCase()}.png`)}
+            className="w-8"
+            alt={value.toLowerCase()} />
+          <img src={require(`../../../Assets/${value.split('_')[1].toLowerCase()}.png`)}
+            className="w-8"
+            alt={value.toLowerCase()} />
+        </li>)
+    }
+    // console.log(value.split('_')[1])
+  }
+
+  const [isDisplayed, toggleDisplay] = useState(false)
+  const sortFilter = useAppSelector(state => state.dataFilter.sortFilter)
   return (
-    <div className="rounded bg-yellow-800 border-2 border-yellow-600 w-full flex justify-center items-start">
-        <select className="rounded ring-0 w-8/12 h-8 bg-stone-800 border-solid border-2 m-2 border-stone-500 text-emerald-400
-      hover:bg-stone-600 hover:border-stone-400 focus:ring-0 focus:bg-stone-700 focus:border-stone-500 outline-0"
-        onChange={(e) => {
-          dispatch(setDataFilter(e.target.value))
-          }}
-        value={sortFilter}>
-        <option value={DataSort.DEFAULT}>{DataSort.DEFAULT.toUpperCase()}</option>
-        <option value={DataSort.HEALTH_DESC}>{DataSort.HEALTH_DESC}</option>
-        <option value={DataSort.HEALTH_ASC}>{DataSort.HEALTH_ASC}</option>
-        <option value={DataSort.COST_DESC}>{DataSort.COST_DESC}</option>
-        <option value={DataSort.COST_ASC}>{DataSort.COST_ASC}</option>
-        <option value={DataSort.ATTACK_DESC}>{DataSort.ATTACK_DESC}</option>
-        <option value={DataSort.ATTACK_ASC}>{DataSort.ATTACK_ASC}</option>
-      </select>
+    <div>
+      <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        onClick={() => {
+          toggleDisplay(!isDisplayed)
+        }}>Filter</button>
+      <div id="dropdownHover" className={`${(isDisplayed ? 'visible' : 'hidden')}
+  z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}>
+        <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownHoverButton">
+          {options}
+        </ul>
+      </div>
     </div>
   )
 }
