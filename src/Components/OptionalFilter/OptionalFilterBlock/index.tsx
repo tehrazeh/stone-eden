67db0596b057f9945a70ec9b3502b0ef8449944e
tehrazeh <danilatolstoukhov@gmail.com>
2023-03-17 21:08:56 -0400

    DRY: created function that return style of the additional filter input, added key to additional filter block

--- src/Components/OptionalFilter/OptionalFilterBlock/index.tsx
+++ src/Components/OptionalFilter/OptionalFilterBlock/index.tsx
@@ -8,15 +8,15 @@ type OptionalBlockProps = {
     item: keyof AdditionalFilters
 }
 
-
 const OptionalFilterBlock: React.FC<OptionalBlockProps> = ({ optionalFilters, item }) => {
-    const inputValid = `bg-stone-900 rounded border-2 text-emerald-200 placeholder-emerald-100
-    placeholder-opacity-50 p-1 border-solid border-emerald-700 focus:border-emerald-600
-    focus:bg-stone-800 focus:outline-none focus:text-emerald-200`
 
-    const inputInvalid = `bg-stone-900 rounded border-2 text-red-200 placeholder-red-100
-    placeholder-opacity-50 p-1 border-solid border-red-700 focus:border-red-600
-    focus:bg-stone-800 focus:outline-none focus:text-red-200`
+    // function that returns style of the input
+    const getInputStyle = (isValid: boolean) => {
+        const color = isValid ? 'emerald' : 'red'
+        return `bg-stone-900 rounded border-2 text-${color}-200 placeholder-${color}-100
+        placeholder-opacity-50 p-1 border-solid border-${color}-700 focus:border-${color}-600
+        focus:bg-stone-800 focus:outline-none focus:text-${color}-200`
+    }
 
     const dispatch = useAppDispatch()
     return (
@@ -28,7 +28,7 @@ const OptionalFilterBlock: React.FC<OptionalBlockProps> = ({ optionalFilters, it
                     alt='attribute'
                     className="w-12 h-11" />
                 <input type='text' placeholder={`Enter ${item}...`}
-                    className={optionalFilters[item].isValid ? inputValid : inputInvalid}
+                    className={getInputStyle(optionalFilters[item].isValid)}
                     value={optionalFilters[item].value}
                     onChange={(e) => {
                         dispatch(setAdditionalFilter({
