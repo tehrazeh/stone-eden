d9ca8d06aee57daaa356efcb9734522b76ab12f8
tehrazeh <danilatolstoukhov@gmail.com>
2023-05-07 15:06:38 -0400

    Increased time for on mouse leave event that hides dropdown filters

--- src/Components/SearchResult/ResultFilter/index.tsx
+++ src/Components/SearchResult/ResultFilter/index.tsx
@@ -20,14 +20,14 @@ const ResultFilter = () => {
      border-neutral-600 w-full mx-4 max-h-[60px] h-[60px] z-30 items-center rounded">
       <div className="w-[20%] h-12 my-1" onMouseLeave={() => setTimeout(() => {
         dispatch(toggleDropdown({ visibility: false, filterType: 'Attribute' }))
-      }, 200)}>
+      }, 350)}>
         <DropdownButton filter={sortFilter}
           filterType='Attribute' filterList={filterList} />
         <SortOptions />
       </div>
       <div className="w-[20%] h-12 my-1" onMouseLeave={() => setTimeout(() => {
         dispatch(toggleDropdown({ visibility: false, filterType: 'Class' }))
-      }, 200)}>
+      }, 350)}>
         <DropdownButton filter={dropdownFilters.classFilter}
           filterType='Class' filterList={filterList} />
         {info?.classes && <FilterOptions options={info.classes}
@@ -35,7 +35,7 @@ const ResultFilter = () => {
       </div>
       <div className="w-[20%] h-12 my-1" onMouseLeave={() => setTimeout(() => {
         dispatch(toggleDropdown({ visibility: false, filterType: 'Type' }))
-      }, 200)}>
+      }, 350)}>
         <DropdownButton filter={dropdownFilters.typeFilter}
           filterType='Type' filterList={filterList} />
         {info?.types && <FilterOptions options={info.types}
