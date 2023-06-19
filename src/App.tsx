4f18de3bee648634bcd3dfd9663d020108cd0b25
tehrazeh <danilatolstoukhov@gmail.com>
2023-06-19 18:52:25 -0400

    Connected custom AppRouter component to make routing tests more convenient

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import './App.css';
-import Footer from './Components/Footer';
-import Header from './Components/Header';
-import { Route, Routes } from 'react-router-dom';
-import Search from './Pages/Search';
+import "./App.css";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import AppRouter from "./routes/AppRouter";
 
 function App() {
   return (
-    <div className='flex flex-col min-h-screen'>
-      <Header/>
-      <div className='flex flex-grow flex-col bg-stone-700'>
-        <Routes>
-          <Route path='/' element={
-                  <div className='bg-amber-900'>
-                  main section my friend
-                </div>
-          } />
-          <Route path='/search/:type' element={<Search />} />
-          <Route path='/filter' element={<div>bro its a test bro</div>}/>
-        </Routes>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="flex flex-grow flex-col bg-stone-700">
+        <AppRouter />
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,20 +1,17 @@
-import { Route, Routes } from 'react-router-dom'
-import Search from '../Pages/Search'
+import { Route, Routes } from "react-router-dom";
+import Search from "../Pages/Search";
 
 const AppRouter = () => {
   return (
-    <div className='flex flex-grow flex-col bg-stone-500'>
     <Routes>
-      <Route path='/' element={
-              <div className='h-full bg-amber-900'>
-              main section my friend
-            </div>
-      } />
-      <Route path='/search/:type' element={<Search />} />
-      <Route path='/filter' element={<div>bro its a test bro</div>}/>
+      <Route
+        path="/"
+        element={<div className="bg-amber-900">main section my friend</div>}
+      />
+      <Route path="/search/:type" element={<Search />} />
+      <Route path="/filter" element={<div>bro its a test bro</div>} />
     </Routes>
-  </div>
-  )
-}
+  );
+};
 
-export default AppRouter
+export default AppRouter;
