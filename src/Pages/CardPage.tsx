c1af4f9099927573f2b59e94a7247c14f6a47b6f
tehrazeh <danilatolstoukhov@gmail.com>
2023-08-10 12:27:42 -0300

    Refactored layout for the card page, cleared console logs

--- src/Pages/CardPage.tsx
+++ src/Pages/CardPage.tsx
@@ -36,22 +36,25 @@ const CardPage = () => {
   const addImageFallback = (event: SyntheticEvent<HTMLImageElement, Event>) => {
     event.currentTarget.src = fallbackImg;
   };
-
-  console.log(card);
   return (
-    <div>
+    <div className="bg-amber-700 bg-opacity-50 min-h-[90vh] flex justify-evenly flex-wrap">
       <button
-        className="border-2 border-lime-400 rounded text-lime-100"
+        className="border-2 border-lime-400 rounded text-lime-100 absolute mt-1 top-auto left-2"
         onClick={() => history(-1)}
       >
         GO BACK BRO
       </button>
-      <div className="w-full bg-amber-500 bg-opacity-50 min-h-[90vh] flex justify-center items-center">
-        <div>
-          <div>
+      <p className="font-bold text-amber-200 text-center tracking-wide w-full  text-3xl">
+        {card.name}
+      </p>
+      <div className="bg-slate-800 flex flex-col justify-between w-[50%] m-2">
+        <div className="bg-amber-800 bg-opacity-25 flex ">
+          <div className="w-[50%]">
             {loaded ? null : <img src={fallbackLazy} alt="card" />}
             <img
-              className={`${loaded ? "w-30" : "hidden"}`}
+              className={`${
+                loaded ? "w-30 border-2 border-red-300" : "hidden"
+              }`}
               src={`https://art.hearthstonejson.com/v1/render/latest/enUS/256x/${card.cardId}.png`}
               alt="card"
               onError={addImageFallback}
@@ -60,24 +63,28 @@ const CardPage = () => {
               }}
             />
           </div>
-          <div>
-            <p>Attack: {card.attack}</p>
-            <p>Health: {card.health}</p>
-            <p>Cost: {card.cost}</p>
-            <p>Text: {card.text}</p>
+          <div className="flex flex-col bg-red-50 justify-evenly items-center w-[50%]">
             <p>Race: {card.race}</p>
             <p>Type: {card.type}</p>
-            <p>{card.name}</p>
             <p>Rarity: {card.rarity}</p>
+            <p>Extension: {card.cardSet}</p>
             <p>Player Class: {card.playerClass}</p>
           </div>
         </div>
-        <div>
-          <img
-            src={`https://art.hearthstonejson.com/v1/orig/${card.cardId}.png`}
-            alt="card art"
-          />
+        <div className="bg-amber-500 bg-opacity-25 h-full">
+          <p>Attack: {card.attack}</p>
+          <p>Health: {card.health}</p>
+          <p>Cost: {card.cost}</p>
+          <p>Text: {card.text}</p>
         </div>
+      </div>
+
+      <div className="bg-stone-900 p-2 rounded-lg h-full m-2">
+        <img
+          className="w-[100%] max-w-[600px] rounded-lg"
+          src={`https://art.hearthstonejson.com/v1/orig/${card.cardId}.png`}
+          alt="card art"
+        />
       </div>
     </div>
   );
